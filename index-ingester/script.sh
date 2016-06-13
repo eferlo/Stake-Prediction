1254e15b056b7e6ac5149cd363145c5e8c988bea
eferlo <eferlo@gmail.com>
2016-06-13 22:04:19 +0200

    Modificaciones en el script de arranque

--- index-ingester/script.sh
+++ index-ingester/script.sh
@@ -6,3 +6,4 @@
 echo "RUNNING JOB"
 spark-submit --class com.utad.index.IndexIngester --master local[2] ~/Stake-Prediction/index-ingester/target/index-ingester-1.0-jar-with-dependencies.jar localhost 9042
 
+spark-submit --class com.utad.index.IndexIngester ~/Stake-Prediction/index-ingester/target/index-ingester-1.0-jar-with-dependencies.jar ec2-54-84-8-248.compute-1.amazonaws.com 9042
